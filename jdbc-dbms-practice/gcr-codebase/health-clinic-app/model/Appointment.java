b265240c753fcbab5877a043b68a184f0e91fc17
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-02-09 21:20:25 +0530

    [Divyansh Bhatt]Add.added jdbc questions

---
+++ jdbc-dbms-practice/gcr-codebase/health-clinic-app/model/Appointment.java
@@ -0,0 +1,19 @@
+public class Appointment{
+
+    private int patientId;
+    private int doctorId;
+    private String date;
+    private String time;
+
+    public Appointment(int patientId,int doctorId,String date,String time){
+        this.patientId=patientId;
+        this.doctorId=doctorId;
+        this.date=date;
+        this.time=time;
+    }
+
+    public int getPatientId(){return patientId;}
+    public int getDoctorId(){return doctorId;}
+    public String getDate(){return date;}
+    public String getTime(){return time;}
+}
